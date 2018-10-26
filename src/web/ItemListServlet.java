9563dd34e6ff8a5183469198860b13585edc80e2
test <g7328318@nwytg.net>
2018-10-26 18:00:17 +0900

    new Created ItemListServlet +a

--- src/web/ItemListServlet.java
+++ src/web/ItemListServlet.java
@@ -1,6 +1,11 @@
 package web;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,12 +25,90 @@ public class ItemListServlet extends HttpServlet {
 		String word = req.getParameter("word");
 		String cat = req.getParameter("cat");
 
+
+
+
+
+	}
+
+	public static ItemBean moldingItem() {
+
+		ItemBean item = null;
+
+		return item;
+
+	}
+
+	public static List<ItemBean> getItemListDAO(String word_, String cat_) {
+
+		String url = "jdbc:mysql://localhost/lesson2018";
+		String id = "root";
+		String pw = "password";
+
+		Connection cnct = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String query = null;
+
 		List<ItemBean> items = new ArrayList();
 
-		ItemBean item = new ItemBean();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			cnct = DriverManager.getConnection(url,id,pw);
+
+			stmt = cnct.createStatement();
+
+			if(word_.isEmpty() || cat_.isEmpty() ) {
+
+			} else if(word_.isEmpty()) {
+			query = "SELECT * FROM product WHERE pro_cd =" + /*num(?)*/ + ";";
+		} else if(cat_.isEmpty()) {
+
+			} else {
+				query = "SELECT * FROM product";
+			}
+
+			rs = stmt.executeQuery(query);
 
 
+			while(rs.next()){
 
+				int pro_cd = rs.getInt("pro_cd");
+				String pro_name = rs.getString("pro_name");
+				int stock_no = rs.getInt("stock_no");
+				int pro_price = rs.getInt("pro_price");
+				int cat_id = rs.getInt("cat_id");
+				String pro_img = rs.getString("pro_img");
+				String pro_msg = rs.getString("pro_msg");
+
+				ItemBean Item = new ItemBean(
+											pro_cd,
+											pro_name,
+											stock_no,
+											pro_price,
+											cat_id,
+											pro_img,
+											pro_msg
+											);
+				items.add(Item);
+			}
+
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (stmt != null) stmt.close();
+				if (cnct != null) cnct.close();
+			} catch (Exception ex) {
+					ex.printStackTrace();
+			}
+		}
+		return items;
 	}
 
 }
