98079f414f2747c09bdec884b5005239f43f632f
EDU-13 <sh>
2018-10-29 17:46:41 +0900

    購入完了画面完成（仮）

---
+++ src/web/ResultServlet.java
@@ -0,0 +1,43 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ResultServlet extends HttpServlet {
+
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	resp.setContentType("text/html;charset=UTF-8");
+
+	  String msg = (String)req.getParameter("msg");
+	  String msg1 = (String)req.getParameter("msg1");
+
+
+
+	if(msg !=null) {
+		 HttpSession session = req.getSession(false);
+
+		    if(session== null)
+		    	return;
+
+
+	}else if(msg1 !=null){
+		HttpSession session = req.getSession(false);
+		if (session != null) {
+		session.invalidate();
+
+
+	RequestDispatcher dispatcher = req.getRequestDispatcher("/jsp/Result.jsp");
+    dispatcher.forward(req, resp);
+
+	}
+   }
+}
+}
